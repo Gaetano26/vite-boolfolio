c253bb4679ea9feec3c8e5e898ebd8f5072f562c
Gaetano26 <gaetano.spampinato@outlook.it>
2023-06-14 15:33:10 +0200

    try fix card

--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -2,7 +2,7 @@
     <div v-if="project">
        <h1>{{ project.title }}</h1>
        <img class="w-50" :src="project.image" :alt="project.title">
-       <p>{{ project.body }}</p>
+       <p  v-html="project.body"></p>
        <h3>{{ project.category.name }}</h3>
         <div class="d-flex gap-3">
             <p v-for=" tag in project.tags"  class="badge rounded-pill text-bg-primary py-2 px-2 mt-2">{{ tag.name }}</p>
